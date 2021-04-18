bf518fbc6a9b7d98abbbfc4dc6ce7663b00c53b1
vimanshu chandra <35292453+vimanshu@users.noreply.github.com>
2021-04-18 23:09:11 +0530

    Create rolling_hash.cpp

---
+++ string/rolling_hash.cpp
@@ -0,0 +1,23 @@
+// making the rolling hash function
+#define ll long long int
+#include <bits/stdc++.h>
+using namespace std;
+int compute_hash(string &s){
+    ll val=0;
+    ll mod=1e9+9;
+    ll p=31;
+    ll p_power=1;
+    for(char c:s){
+        val=(val+(c-'a'+1)*p_power)%mod;
+        p_power=p*p_power;
+    }
+    return val;
+}
+
+int main()
+{
+    string s;
+    cin>>s;
+   cout<< compute_hash(s);
+    return 0;
+}
