c4499045757da6b524b01984105543201e101425
vimanshu chandra <35292453+vimanshu@users.noreply.github.com>
2021-04-19 00:17:49 +0530

    Create rabin_karp_string_match.cpp

---
+++ string/rabin_karp_string_match.cpp
@@ -0,0 +1,50 @@
+// rabin karp algorithm for string matching
+// question- given two strings, onr text one pattern, determine if the pattern appears in the text and if exists then enumerate all the occurances
+// rolling hash function== s[i]*p**[i]%mod;
+
+#define ll long long int
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> string_match(string t, string pat,vector <int> &occurences){
+    int p=31;
+    int mod=1e9+9;
+    int T=t.size();
+    int P=pat.size();
+    vector<long long> p_pow(max(T,P));
+    p_pow[0]=1;
+    for (int i = 1; i < p_pow.size(); i++) {
+        p_pow[i]=p_pow[i-1]*p%mod;
+    }
+    vector<long long>  h(T+1,0);
+    for(int i=0; i<T;i++){
+        h[i+1]=h[i]+(t[i]-'a'+1)*p_pow[i]%mod;
+    }
+    ll h_p=0;
+    for(int i=0; i<P;i++){
+        h_p+=(pat[i]-'a'+1)*p_pow[i]%mod;
+    }
+    
+    for(int i=0; i+P-1<T;i++){
+        ll curr_hash=(h[i+P]+mod-h[i])%mod;
+        if(curr_hash==h_p*p_pow[i]%mod){
+            occurences.push_back(i);
+        }
+        
+    }
+    return occurences;
+}
+int main()
+{
+    string t,p; // p->pattern, text
+    cin>>t>>p;
+    vector<int> occurences;
+    string_match(t,p,occurences);
+    
+    for (int i = 0; i < occurences.size(); i++) {
+        cout<<occurences[i]<<" ";
+    }
+
+    return 0;
+}
